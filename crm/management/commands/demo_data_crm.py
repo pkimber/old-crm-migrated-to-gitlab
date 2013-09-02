74f6e76be007256cc034ead471c539a512d8f052
Patrick Kimber <code@pkimber.net>
2013-09-02 18:38:04 +0100

    Tests are passing

--- crm/management/commands/demo_data_crm.py
+++ crm/management/commands/demo_data_crm.py
@@ -1,6 +1,6 @@
 from django.core.management.base import BaseCommand
 
-from crm.tests.model_maker import make_section
+from crm.tests.model_maker import make_contact
 
 
 class Command(BaseCommand):
@@ -8,7 +8,5 @@ class Command(BaseCommand):
     help = "Create demo data for 'crm'"
 
     def handle(self, *args, **options):
-        make_section('home')
-        make_section('portfolio')
-        make_section('tech')
+        make_contact('pkimber', 'Patrick Kimber')
         print("Created 'crm' demo data...")
--- crm/models.py
+++ crm/models.py
@@ -8,17 +8,19 @@ from base.model_utils import TimeStampedModel
 class Contact(TimeStampedModel):
     """ Contact """
     name = models.CharField(max_length=100)
-    address = models.TextField()
+    address = models.TextField(blank=True)
+    postcode = models.CharField(max_length=10, blank=True)
+    slug = models.SlugField()
     url = models.URLField(blank=True, null=True)
-    phone = models.CharField(max_length=100)
-    mail = models.EmailField()
+    phone = models.CharField(max_length=100, blank=True)
+    mail = models.EmailField(blank=True)
 
     class Meta:
-        ordering = ['section', 'order', 'modified']
+        ordering = ['slug']
         verbose_name = 'Contact'
         verbose_name_plural = 'Contacts'
 
     def __unicode__(self):
         return unicode('{}'.format(self.name))
 
-reversion.register(Simple)
+reversion.register(Contact)
