1101c34cb50e43919d357cdcad7fad5e52c01bdc
Patrick Kimber <code@pkimber.net>
2013-09-04 19:11:16 +0100

    Test and tidy create ticket and note

--- crm/views.py
+++ crm/views.py
@@ -5,6 +5,11 @@ from django.views.generic import (
 
 from braces.views import LoginRequiredMixin
 
+from .forms import (
+    NoteForm,
+    TicketForm,
+)
+
 from .models import (
     Contact,
     Note,
@@ -29,29 +34,45 @@ class HomeListView(LoginRequiredMixin, ListView):
 
 
 class NoteCreateView(LoginRequiredMixin, CreateView):
+    form_class = NoteForm
     model = Note
 
-    def _get_ticket_pk(self):
+    def _get_ticket(self):
         pk = self.kwargs.get('pk')
         return get_object_or_404(Ticket, pk=pk)
 
+    def get_context_data(self, **kwargs):
+        context = super(NoteCreateView, self).get_context_data(**kwargs)
+        context.update(dict(
+            ticket=self._get_ticket(),
+        ))
+        return context
+
     def form_valid(self, form):
         self.object = form.save(commit=False)
-        self.object.company = self._get_ticket_pk()
+        self.object.ticket = self._get_ticket()
         self.object.user = self.request.user
         return super(NoteCreateView, self).form_valid(form)
 
 
 class TicketCreateView(LoginRequiredMixin, CreateView):
+    form_class = TicketForm
     model = Ticket
 
-    def _get_contact_slug(self):
+    def _get_contact(self):
         slug = self.kwargs.get('slug')
         return get_object_or_404(Contact, slug=slug)
 
+    def get_context_data(self, **kwargs):
+        context = super(TicketCreateView, self).get_context_data(**kwargs)
+        context.update(dict(
+            contact=self._get_contact(),
+        ))
+        return context
+
     def form_valid(self, form):
         self.object = form.save(commit=False)
-        self.object.company = self._get_contact_slug()
+        self.object.contact = self._get_contact()
         self.object.user = self.request.user
         return super(TicketCreateView, self).form_valid(form)
 
