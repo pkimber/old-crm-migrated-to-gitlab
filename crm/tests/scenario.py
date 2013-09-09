1b58bf909e77739cd2992038c1de8f479f4fc716
Patrick Kimber <code@pkimber.net>
2013-09-09 20:22:42 +0100

    Update test to use standard scenario

--- crm/tests/scenario.py
+++ crm/tests/scenario.py
@@ -1,32 +1,61 @@
 from crm.models import (
     Contact,
+    Note,
+    Ticket,
 )
 from crm.tests.model_maker import (
     make_contact,
+    make_note,
+    make_priority,
+    make_ticket,
     make_user_contact,
 )
 from login.tests.scenario import (
-    get_fred,
-    get_sara,
+    get_user_fred,
+    get_user_sara,
+    get_user_staff,
 )
 
 
 def contact_contractor():
+    fred = get_user_fred()
+    sara = get_user_sara()
+    staff = get_user_staff()
     farm = make_contact(
         'farm',
         "Fred's Farm",
     )
-    make_user_contact(get_fred(), farm)
+    make_user_contact(fred, farm)
+    fence = make_ticket(
+        farm, fred, 'Fence the orchard', make_priority('High', 1)
+    )
+    make_note(
+        fence,
+        staff,
+        'Forgot to order fence posts',
+    )
     smallholding = make_contact(
         'smallholding',
         "Sara's Smallholding",
     )
-    make_user_contact(get_sara(), smallholding)
+    make_user_contact(sara, smallholding)
 
 
-def get_smallholding():
+def get_contact_smallholding():
     return Contact.objects.get(slug='smallholder')
 
 
-def get_farm():
+def get_contact_farm():
     return Contact.objects.get(slug='farm')
+
+
+def get_note_fence_forgot():
+    fence = get_ticket_fence()
+    staff = get_user_staff()
+    return Note.objects.get(ticket=fence, user=staff)
+
+
+def get_ticket_fence():
+    fred = get_user_fred()
+    farm = get_contact_farm()
+    return Ticket.objects.get(contact=farm, user=fred)
--- crm/tests/test_note.py
+++ crm/tests/test_note.py
@@ -3,27 +3,23 @@ from datetime import timedelta
 
 from django.test import TestCase
 
-from crm.tests.model_maker import (
-    make_contact,
-    make_note,
-    make_priority,
-    make_ticket,
+from crm.tests.scenario import (
+    contact_contractor,
+    get_note_fence_forgot,
 )
-from login.tests.model_maker import make_user
+from login.tests.scenario import (
+    user_contractor,
+    user_default,
+)
 
 
 class TestNote(TestCase):
 
     def setUp(self):
-        icl = make_contact('icl', 'ICL')
-        sew = make_ticket(
-            icl, make_user('tom'), 'Sew', make_priority('Low', 1)
-        )
-        self.note = make_note(
-            sew,
-            make_user('zed'),
-            'Cut out some material and make a pillow case',
-        )
+        user_contractor()
+        user_default()
+        contact_contractor()
+        self.note = get_note_fence_forgot()
 
     def test_modified_today(self):
         self.assertTrue(self.note.modified_today())
