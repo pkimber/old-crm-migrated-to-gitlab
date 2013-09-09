ff9e0ec550b269c79965680acdca04619a8b11dd
Patrick Kimber <code@pkimber.net>
2013-09-09 10:43:24 +0100

    Missing settings and requirements

--- example/base.py
+++ example/base.py
@@ -43,7 +43,8 @@ USE_TZ = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = ''
+MEDIA_ROOT = 'media'
+MEDIA_ROOT_PRIVATE = 'media-private'
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
