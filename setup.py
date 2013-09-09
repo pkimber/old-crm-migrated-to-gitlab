6d085b8dabfa7e1a9eeffb072926fe7a0503be4a
Patrick Kimber <code@pkimber.net>
2013-09-09 14:24:02 +0100

    version 0.0.05

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(
             'templates/crm/*.*',
         ],
     },
-    version='0.0.04',
+    version='0.0.05',
     description='crm',
     author='Patrick Kimber',
     author_email='code@pkimber.net',
