2cf91f4a1ec85d802a6fe13baec2e4c2a1fa9cb9
Patrick Kimber <code@pkimber.net>
2013-09-09 11:42:35 +0100

    Will be using 'django-sendfile' for serving private media

--- example/base.py
+++ example/base.py
@@ -44,7 +44,6 @@ USE_TZ = True
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
 MEDIA_ROOT = 'media'
-MEDIA_ROOT_PRIVATE = 'media-private'
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
@@ -170,3 +169,7 @@ DEBUG_TOOLBAR_CONFIG = {
     'INTERCEPT_REDIRECTS': False,
     'ENABLE_STACKTRACES': True,
 }
+
+# https://github.com/johnsensible/django-sendfile
+SENDFILE_BACKEND = 'sendfile.backends.development'
+SENDFILE_ROOT = 'media-private'
