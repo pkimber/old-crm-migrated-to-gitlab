936ed14c8c8cd216289fbfdf8119c7dc7e713099
Patrick Kimber <code@pkimber.net>
2014-02-07 16:02:43 +0000

    update scenario

--- crm/tests/scenario.py
+++ crm/tests/scenario.py
@@ -3,6 +3,7 @@ from decimal import Decimal
 from crm.models import (
     Contact,
     Note,
+    Priority,
     Ticket,
 )
 from crm.tests.model_maker import (
@@ -14,6 +15,7 @@ from crm.tests.model_maker import (
 )
 from login.tests.scenario import (
     get_user_fred,
+    get_user_mike,
     get_user_sara,
     get_user_staff,
 )
@@ -26,10 +28,13 @@ def default_scenario_crm():
     but we forgot to order the fence posts!
 
     sara has a smallholding
+
+    mike owns the local agricultural merchants
     """
     unassigned = make_priority('Unassigned', 0)
     staff = get_user_staff()
     fred = get_user_fred()
+    mike = get_user_mike()
     staff = get_user_staff()
     farm = make_contact(
         'farm',
@@ -58,14 +63,24 @@ def default_scenario_crm():
     )
     make_user_contact(sara, smallholding)
     make_ticket(smallholding, sara, 'Send paperwork to accountant', unassigned)
-
-
-def get_contact_smallholding():
-    return Contact.objects.get(slug='smallholding')
+    merchant = make_contact(
+        'merchant',
+        "Mike's Agricultural Merchants",
+        hourly_rate=Decimal('43.21'),
+    )
+    make_user_contact(mike, merchant)
 
 
 def get_contact_farm():
     return Contact.objects.get(slug='farm')
+
+
+def get_contact_merchant():
+    return Contact.objects.get(slug='merchant')
+
+
+def get_contact_smallholding():
+    return Contact.objects.get(slug='smallholding')
 
 
 def get_note_fence_forgot():
@@ -74,10 +89,21 @@ def get_note_fence_forgot():
     return Note.objects.get(ticket=fence, user=staff)
 
 
+def get_priority_unassigned():
+    return Priority.objects.get(level=0)
+
+
 def get_ticket_fence_for_farm():
     fred = get_user_fred()
     farm = get_contact_farm()
     return Ticket.objects.get(contact=farm, user=fred)
+
+
+def get_ticket_fix_roof_for_farm():
+    staff = get_user_staff()
+    farm = get_contact_farm()
+    priority=get_priority_unassigned()
+    return Ticket.objects.get(contact=farm, user=staff, priority=priority)
 
 
 def get_ticket_paperwork_for_smallholding():
