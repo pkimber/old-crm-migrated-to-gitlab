ad9bdf0bb1fd485cb6a7f5afeb392b4612a3e279
Patrick Kimber <code@pkimber.net>
2014-07-16 14:58:02 +0100

    factory - hourly rate

--- crm/tests/factories.py
+++ crm/tests/factories.py
@@ -1,6 +1,8 @@
 # -*- encoding: utf-8 -*-
 from __future__ import unicode_literals
 
+from decimal import Decimal
+
 from django.template.defaultfilters import slugify
 
 import factory
@@ -12,6 +14,8 @@ class ContactFactory(factory.django.DjangoModelFactory):
 
     class Meta:
         model = Contact
+
+    hourly_rate = Decimal('20.00')
 
     @factory.sequence
     def slug(n):
