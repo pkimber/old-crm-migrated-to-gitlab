c953101ee40cf92096c8fcfdc28e1284cb7694ef
Patrick Kimber <code@pkimber.net>
2014-08-03 15:14:07 +0100

    version 0.1.06

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ setup(
             'templates/search/indexes/crm/*.*',
         ],
     },
-    version='0.1.05',
+    version='0.1.06',
     description='crm',
     author='Patrick Kimber',
     author_email='code@pkimber.net',
