5f841a6e4f390d7ee4da0c5958a507555b4a5344
Patrick Kimber <code@pkimber.net>
2015-06-13 18:13:17 +0100

    tidy

--- crm/views.py
+++ crm/views.py
@@ -19,7 +19,6 @@ from braces.views import (
 from rest_framework import (
     authentication,
     permissions,
-    status,
 )
 from rest_framework.response import Response
 from rest_framework.views import APIView
