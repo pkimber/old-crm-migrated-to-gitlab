e406c97dfbdf7dae75cb4f25d2d6ec4d8f228f28
Patrick Kimber <code@pkimber.net>
2016-01-25 12:27:00 +0000

    contact app - migration 4 - #717

--- crm/admin.py
+++ crm/admin.py
@@ -1,13 +1,11 @@
 # -*- encoding: utf-8 -*-
-
-from __future__ import unicode_literals
 from django.contrib import admin
 
 from .models import (
     Contact,
     Industry,
     Priority,
-    UserContact,
+    # UserContact,
 )
 
 
@@ -29,20 +27,20 @@ class PriorityAdmin(admin.ModelAdmin):
 admin.site.register(Priority, PriorityAdmin)
 
 
-class UserContactAdmin(admin.ModelAdmin):
-    list_display = ('get_username', 'get_contact', 'get_contact_address')
-    ordering = ['user__username']
-
-    def get_contact(self, obj):
-        return '{}'.format(obj.contact.name)
-    get_contact.short_description = 'Contact'
-
-    def get_contact_address(self, obj):
-        return '{}'.format(obj.contact.address)
-    get_contact_address.short_description = 'Address'
-
-    def get_username(self, obj):
-        return '{}'.format(obj.user.username)
-    get_username.short_description = 'User Name'
-
-admin.site.register(UserContact, UserContactAdmin)
+# class UserContactAdmin(admin.ModelAdmin):
+#     list_display = ('get_username', 'get_contact', 'get_contact_address')
+#     ordering = ['user__username']
+#
+#     def get_contact(self, obj):
+#         return '{}'.format(obj.contact.name)
+#     get_contact.short_description = 'Contact'
+#
+#     def get_contact_address(self, obj):
+#         return '{}'.format(obj.contact.address)
+#     get_contact_address.short_description = 'Address'
+#
+#     def get_username(self, obj):
+#         return '{}'.format(obj.user.username)
+#     get_username.short_description = 'User Name'
+#
+# admin.site.register(UserContact, UserContactAdmin)
--- crm/models.py
+++ crm/models.py
@@ -83,32 +83,34 @@ class CrmContact(TimeStampedModel):
 reversion.register(CrmContact)
 
 
-class UserContact(TimeStampedModel):
-    """
-    A user is linked to a single contact.
-    More than one user can link to the same contact, but a user cannot
-    link to more than one contact.
-
-    e.g.
-    Andy - ConnexionSW
-    Fred - ConnexionSW
-    Kate - British Sugar
-
-    """
-
-    user = models.OneToOneField(settings.AUTH_USER_MODEL, related_name='user')
-    contact = models.ForeignKey(Contact)
-    # PJK1
-    # new_contact = models.ForeignKey(settings.CONTACT_MODEL, blank=True, null=True, related_name='usercontact_contact')
-
-    # contact = models.ForeignKey(settings.CONTACT_MODEL, related_name='contact')
-    # crm_contact = models.ForeignKey(Contact)
-    # crm_contact = models.ForeignKey(Contact) #, related_name='crm_contact_user_contact')
-    # PJK2
-    # contact = models.ForeignKey(settings.CONTACT_MODEL, blank=True, null=True, related_name='user_contact_contact')
-
-    def __str__(self):
-        return '{} - {}'.format(self.user.username, self.contact.name)
+# class UserContact(TimeStampedModel):
+#     """
+#     A user is linked to a single contact.
+#     More than one user can link to the same contact, but a user cannot
+#     link to more than one contact.
+#
+#     e.g.
+#     Andy - ConnexionSW
+#     Fred - ConnexionSW
+#     Kate - British Sugar
+#
+#     25/01/2016 Is not used on the KB CRM system, so we will remove it.
+#
+#     """
+#
+#     user = models.OneToOneField(settings.AUTH_USER_MODEL, related_name='user')
+#     contact = models.ForeignKey(Contact)
+#     # PJK1
+#     # new_contact = models.ForeignKey(settings.CONTACT_MODEL, blank=True, null=True, related_name='usercontact_contact')
+#
+#     # contact = models.ForeignKey(settings.CONTACT_MODEL, related_name='contact')
+#     # crm_contact = models.ForeignKey(Contact)
+#     # crm_contact = models.ForeignKey(Contact) #, related_name='crm_contact_user_contact')
+#     # PJK2
+#     # contact = models.ForeignKey(settings.CONTACT_MODEL, blank=True, null=True, related_name='user_contact_contact')
+#
+#     def __str__(self):
+#         return '{} - {}'.format(self.user.username, self.contact.name)
 
 
 class Priority(models.Model):
@@ -157,7 +159,9 @@ class Ticket(TimeStampedModel):
     # PJK2
     # new_contact = models.ForeignKey(settings.CONTACT_MODEL, blank=True, null=True, related_name='ticket_contact')
     # PJK3
-    new_contact = models.ForeignKey(settings.CONTACT_MODEL, related_name='ticket_contact')
+    # new_contact = models.ForeignKey(settings.CONTACT_MODEL, related_name='ticket_contact')
+    # PJK4
+    contact = models.ForeignKey(settings.CONTACT_MODEL, related_name='ticket_contact')
 
     # contact = models.ForeignKey(settings.CONTACT_MODEL, blank=True, null=True)
     # contact = models.ForeignKey(settings.CONTACT_MODEL)
